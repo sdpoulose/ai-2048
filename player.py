8f98d2cf416a5043674ece50ad75ce6107a4b59f
Solomon Poulose <solomon@localhost.localdomain>
2020-10-27 20:49:04 -0400

    added correct ranges to movement methods

--- player.py
+++ player.py
@@ -1,14 +1,34 @@
 def move_up(game_state):
-    pass
+    for i in range(5, 9):
+        pass
+    for i in range(9, 13):
+        pass
+    for i in range(13, 17):
+        pass
 
 
 def move_down(game_state):
-    pass
+    for i in range(9, 13):
+        pass
+    for i in range(5, 9):
+        pass
+    for i in range(1, 5):
+        pass
 
 
 def move_left(game_state):
-    pass
+    for i in range(2, 15, 4):
+        pass
+    for i in range(3, 16, 4):
+        pass
+    for i in range(4, 17, 4):
+        pass
 
 
 def move_right(game_state):
-    pass
+    for i in range(3, 16, 4):
+        pass
+    for i in range(2, 15, 4):
+        pass
+    for i in range(1, 14, 4):
+        pass
