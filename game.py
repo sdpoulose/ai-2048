751eef78324a5b3ab0ba980a7f17c9efee49b8f4
Solomon Poulose <solomon@localhost.localdomain>
2020-10-27 20:31:48 -0400

    added randomly spawning tiles

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from tile import *
+import random
 
 game_state = [tile_1, tile_2, tile_3, tile_4,
               tile_5, tile_6, tile_7, tile_8,
@@ -6,6 +7,18 @@ game_state = [tile_1, tile_2, tile_3, tile_4,
               tile_13, tile_14, tile_15, tile_16]
 
 game_on = True
+
+
+def get_blanks(game_state):
+    blank_tiles = []
+    for tile in game_state:
+        if tile.value == 0:
+            blank_tiles.append(tile.id)
+    new_tile_id = random.randint(1, len(blank_tiles))
+    if random.randint(1, 5) == 1:
+        game_state[new_tile_id].value = 4
+    else:
+        game_state[new_tile_id].value = 2
 
 
 def check_game_on(game_state):
@@ -27,19 +40,19 @@ def refresh_game(game_state, game_on):
     print("_____________")
 
 
-def move_up():
+def move_up(game_state):
     pass
 
 
-def move_down():
+def move_down(game_state):
     pass
 
 
-def move_left():
+def move_left(game_state):
     pass
 
 
-def move_right():
+def move_right(game_state):
     pass
 
 
@@ -51,3 +64,4 @@ while(1):
     next_move = input()
     if next_move == 'q':
         game_on = False
+    get_blanks(game_state)
