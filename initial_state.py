32148d3d4ec5776a582fb92e6ca72f585146924c
sdpoulose <sdpoulose@gmail.com>
2021-01-15 18:28:28 -0500

    code clarification of initial state creation

--- initial_state.py
+++ initial_state.py
@@ -1,8 +1,13 @@
 from tile import *
 
+# store the value and position of all tiles into an array
+# and maintain it as the game state
+
 game_state = [tile_1, tile_2, tile_3, tile_4,
               tile_5, tile_6, tile_7, tile_8,
               tile_9, tile_10, tile_11, tile_12,
               tile_13, tile_14, tile_15, tile_16]
+
+# bool to check if end game conditions have been met
 
 game_on = True
