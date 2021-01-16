b87c12901a9b81e4734879e6d7501ec59b2d8a37
sdpoulose <sdpoulose@gmail.com>
2021-01-16 18:17:18 -0500

    modified ai logic

--- ai.py
+++ ai.py
@@ -26,7 +26,7 @@ def ai_first_move_up(game_state):
     move_up(game_state_first_up)
     get_blanks(game_state_first_up)
 
-    ai_sequent_moves(game_state_first_up)
+    ai_sequent_moves(game_state_first_up, up_max)
 
 
 def ai_first_move_down(game_state):
@@ -34,7 +34,7 @@ def ai_first_move_down(game_state):
     move_up(game_state_first_down)
     get_blanks(game_state_first_down)
 
-    ai_sequent_moves(game_state_first_down)
+    ai_sequent_moves(game_state_first_down, down_max)
 
 
 def ai_first_move_right(game_state):
@@ -42,7 +42,7 @@ def ai_first_move_right(game_state):
     move_up(game_state_first_right)
     get_blanks(game_state_first_right)
 
-    ai_sequent_moves(game_state_first_right)
+    ai_sequent_moves(game_state_first_right, right_max)
 
 
 def ai_first_move_left(game_state):
@@ -50,16 +50,30 @@ def ai_first_move_left(game_state):
     move_up(game_state_first_left)
     get_blanks(game_state_first_left)
 
-    ai_sequent_moves(game_state_first_left)
+    ai_sequent_moves(game_state_first_left, left_max)
 
 
 # recursive function for all possible moves
-def ai_sequent_moves(game_state):
-    while(level < 5):
-        game_state_up = copy.deepcopy(game_state)
-        game_state_down = copy.deepcopy(game_state)
-        game_state_left = copy.deepcopy(game_state)
-        game_state_right = copy.deepcopy(game_state)
+def ai_sequent_moves(game_state, max_list):
+
+    global level
+    level += 1
+
+    game_state_up = copy.deepcopy(game_state)
+    game_state_down = copy.deepcopy(game_state)
+    game_state_left = copy.deepcopy(game_state)
+    game_state_right = copy.deepcopy(game_state)
+
+    move_up(game_state_up)
+    move_down(game_state_down)
+    move_left(game_state_left)
+    move_right(game_state_right)
+
+    get_blanks(game_state_up)
+    get_blanks(game_state_down)
+    get_blanks(game_state_left)
+    get_blanks(game_state_right)
+
 
 # calculate most favorable first move and apply it
 
