5333a0229820d3066998933a478352b646f3460a
sdpoulose <sdpoulose@gmail.com>
2021-01-20 20:28:36 -0500

    added function to simulate random moves for 10 steps

--- random_ai.py
+++ random_ai.py
@@ -1,3 +1,24 @@
 from game_functions import *
 import time
 import copy
+import random
+
+up_max = []
+down_max = []
+left_max = []
+right_max = []
+
+
+def simulate_first_move_up(game_state):
+    game_state_up = copy.deepcopy(game_state)
+    move_up(game_state)
+    for iter in range(10):
+        next_move = random.randint(1, 4)
+        if next_move == 1:
+            move_up(game_state_up)
+        elif next_move == 2:
+            move_down(game_state_up)
+        elif next_move == 3:
+            move_left(game_state_up)
+        else:
+            move_right(game_state_up)
